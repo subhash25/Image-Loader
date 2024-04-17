74f83a6434edfe4492aead4876cf6859b0f797ab
subhash2525 <subhashbicholkar@gmail.com>
2024-04-17 11:11:46 +0530

    image loading optimization

--- app/src/main/java/assignment/imageloader/ListAdapter.kt
+++ app/src/main/java/assignment/imageloader/ListAdapter.kt
@@ -10,14 +10,20 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class ListAdapter(private val dataList: List<ImageDataModelItem>,private val context: Context) :
     RecyclerView.Adapter<ListAdapter.ViewHolder>() {
     private val imageLoader = EfficientImageLoader(context)
+    // Coroutine scope for managing image loading tasks
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
     // ViewHolder class to hold references to views
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageview)
+        var currentImageUrl = ""
+        var currentJob: Job? = null
     }
 
     // Create new views (invoked by the layout manager)
@@ -30,21 +36,31 @@ class ListAdapter(private val dataList: List<ImageDataModelItem>,private val con
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val imageThumbnail = dataList[position].thumbnail
-        val imageUrl =
+        holder.currentImageUrl =
             "${imageThumbnail.domain}/${imageThumbnail.basePath}/0/${imageThumbnail.key}"
-        //holder.imageView.setImageBitmap(null)
-        CoroutineScope(Dispatchers.Main).launch {
-            val bitmap = imageLoader.loadImage(imageUrl)
+        holder.imageView.setImageResource(R.drawable.placeholder)
+        // Cancel previous coroutine job if it exists
+        holder.currentJob?.cancel()
+        val job = coroutineScope.launch {
+            val bitmap = imageLoader.loadImage(holder.currentImageUrl)
             bitmap?.let {
                 holder.imageView.setImageBitmap(it)
             } ?: run {
                 // Set placeholder image or handle error
-                holder.imageView.setImageResource(R.drawable.placeholder)
+                holder.imageView.setImageResource(R.drawable.picture_loading_failed)
             }
         }
+        // Store the job in the ViewHolder
+        holder.currentJob = job
     }
 
     override fun getItemCount(): Int {
         return dataList.size
+    }
+
+    // Cancel coroutine scope when adapter is destroyed
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        coroutineScope.cancel()
     }
 }
